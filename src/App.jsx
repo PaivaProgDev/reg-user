ff091cf4b82cc91aad366a454e52580cb73860e4
Guilherme <guipaiva.ferreira2021@gmail.com>
2025-04-29 16:38:55 -0300

    Header finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import "./App.css";
 
 const App = () => {
   return (
-    <main>
+    <main className="App w-full max-w-[100rem] m-auto">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/user-list" />
           <Route path="/user-registration" />
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-[#191919] p-2 text-white">
+      <h1 className="text-2xl">
+        <span className="font-extralight">Reg</span>
+        <span className="font-semibold">User</span>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
